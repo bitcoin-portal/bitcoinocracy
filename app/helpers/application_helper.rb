44785888cad1157406010e0c61169310d45716e9
Sam Cheng Hung <samchenghung@gmail.com>
2017-03-06 16:17:20 +0800

    Adds thousands delimiter for winning argument number

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,10 @@ module ApplicationHelper
     "#{options[:prefix]}%.8f#{options[:suffix]}".html_safe % (1.0*satoshies.to_i/1e8)
 	end
 
+	def btc_human_spaced(satoshies, options = {prefix: '', suffix: ' BTC'} )
+		number_to_currency(1.0*satoshies.to_i/1e8, unit: options[:suffix], precision: 8, delimiter: " ", format: "%n %u").html_safe
+	end
+
   def percent_human(x)
     if x.present?
       "(%.2f%%)" % (100.0*x)
